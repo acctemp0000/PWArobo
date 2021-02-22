c852c58d4960262a953cba3615f0e7c919765ab5
acctemp0000 <acctemp0000@gmail.com>
2021-02-22 23:25:49 +0530

    Updates

---
+++ precache-manifest.f146f624a63933f0f5ca3cd9fa064764.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ac24a4f897cb52134d32974d88419a1",
+    "url": "/PWArobo/index.html"
+  },
+  {
+    "revision": "b09d20b975196d7d8093",
+    "url": "/PWArobo/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "6a1aa0bfc5167101a48b",
+    "url": "/PWArobo/static/css/main.890bbd40.chunk.css"
+  },
+  {
+    "revision": "b09d20b975196d7d8093",
+    "url": "/PWArobo/static/js/2.277841f7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/PWArobo/static/js/2.277841f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a1aa0bfc5167101a48b",
+    "url": "/PWArobo/static/js/main.740494b5.chunk.js"
+  },
+  {
+    "revision": "99186ad0ec228536c1ee",
+    "url": "/PWArobo/static/js/runtime-main.9470c0d2.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/PWArobo/static/media/SEGA.12a3db8b.woff"
+  }
+]);
